af7461b715767d4d89e15500ac49fec5baa55a74
Jason McCarty <jmccart480@gmail.com>
2022-06-06 19:43:04 -0400

    Files updated with new code

---
+++ AddressRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluralsightPractice_ACM.BL
+{
+    internal class AddressRepository
+    {
+        public Address Retrieve(int addressId)
+        {
+            Address address = new Address(addressId);
+
+            if (addressId == 1)
+            {
+                address.AddressType = 1;
+                address.Street1 = "bag End";
+                address.Street2 = "Bagshot row";
+                address.City = "Hobbiton";
+                address.State = "Shire";
+                address.Country = "Middle Earth";
+                address.PostalCode = "144";
+            }
+            return address;
+        }
+        public IEnumerable<Address> RetrieveByCustomerId(int customerId)
+        {
+            var addressList = new List<Address>();
+            Address addressr = new Address(1);
+        }
+        public bool Save(Address address)
+        {
+            return true;
+        }
+    }
+}
