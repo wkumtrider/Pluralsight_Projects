8ac5a932f145bd644cf3eb22ba585a1ab326c140
Jason McCarty <jmccart480@gmail.com>
2022-06-06 18:48:21 -0400

    Adding additional classes and test files.  Also added additional projects.

---
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+            Customer customer = new Customer();
+
+            customer.FirstName = "Bilbo";
+            customer.LastName = "Baggins";
+            string expected = "Baggins, Bilbo";
+
+            //-- Act
+            string actual = customer.FullName;
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {    //-- Arrange
+            Customer customer = new Customer();
+
+            customer.LastName = "Baggins";
+            string expected = "Baggins";
+
+            //-- Act
+            string actual = customer.FullName;
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo"
+            };
+                    
+            string expected = "Bilbo";
+
+            //-- Act
+            string actual = customer.FullName;
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void StaticTest()
+        {
+            var c1 = new Cuustomer();
+            c1.FirstName = "Frodo";
+            Customer.InstanceCount += 1;
+        }
+        
+        public void ValidateValid()
+        {
+            var customer = new Customer
+            {
+                LastName = "Baggins",
+                EmailAddress = "fbaggins@hobbiton.me";
+            }
+        var actual = Customer.Validate();
+
+        Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Tests/ACM.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("05c06c78-22f6-49a5-ba08-1117aa463041")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
